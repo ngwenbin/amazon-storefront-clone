b3dde673c818f871fa8b8649c138c37932ea5e9b
Wen Bin <wenbin_ng@yahoo.com.sg>
2023-01-21 16:09:37 +0800

    feat: added card component

---
+++ components/Card.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { ProductObject } from "../utils";
+
+interface CardProps {
+  data: ProductObject;
+  onClickCb?: (id: string) => void;
+}
+
+const Card = ({ data, onClickCb }: CardProps) => {
+  const onClickHandler = (id: string) => onClickCb && onClickCb(id);
+  return (
+    <div
+      className="border rounded-[4px] border-[rgba(0,0,0,0.1)] w-min h-[337px]"
+      onClick={() => onClickHandler(data.id)}
+      aria-hidden="true"
+    >
+      <div className="w-[225px] aspect-square bg-[#EEF0FA]">
+        {data?.media && data.media.length > 0 ? (
+          <img
+            src={data.media[0].src}
+            alt=""
+            className="w-full h-full object-cover p-4"
+          />
+        ) : null}
+      </div>
+      <div className="flex flex-col px-4 pt-4">
+        <p className="font-medium text-xs text-[#6f7999]">{data.skuId}</p>
+        <p className="font-semibold text-sm text-[#000]">{data.name}</p>
+        <p className="font-medium text-xs text-[#6f7999]">{data.brand}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
